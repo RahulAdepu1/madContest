81930d2695ff0f51661f07e534017e4ed1463afb
RahulAdepu1 <67685274+RahulAdepu1@users.noreply.github.com>
2023-04-03 13:53:19 -0500

    Initial Commit

---
+++ groceryCalculator/groceryCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  groceryCalculatorApp.swift
+//  groceryCalculator
+//
+//  Created by Rahul Adepu on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct groceryCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
